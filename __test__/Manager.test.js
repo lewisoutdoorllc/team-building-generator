552a08e63a385abd8242ed804fb45fc04b084196
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-02-24 14:58:50 -0500

    minor changes

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -3,9 +3,9 @@ const Manager = require('../lib/Manager');
 test('create a new employee object', () => {
     const manager = new Manager('Cody', '1', 'test@test.com', 'Manager', '25');
 
-    expect(manager.name).toBe('Cody');
-    expect(manager.id).toBe('1');
-    expect(manager.email).toBe('test@test.com');
-    expect(manager.officeNumber).toBe('25');
+    expect(manager.getName()).toBe('Cody');
+    expect(manager.getId()).toBe('1');
+    expect(manager.getEmail()).toBe('test@test.com');
+    expect(manager.getOfficeNumber()).toBe('25');
     expect(manager.getRole()).toBe('Manager');
 });
